a5bfebe272fb7dd1ee5ec2de63cd267992f3fbfe
Victor Nwachukwu <vixkudos@gmail.com>
2025-09-14 22:30:29 +0100

    Basic Arithmetic Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum_result = number1 + number2
+print("Addition of", number1, "and", number2, "is", sum_result)
+
+# Subtraction
+difference_result = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", difference_result)
+
+# Multiplication
+product_result = number1 * number2 
+print("Multiplication of", number1, "and", number2, "is", product_result)
