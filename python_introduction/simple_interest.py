1efa3ff9e6a2bafe7872e5aea6ff1495df25d724
Victor Nwachukwu <vixkudos@gmail.com>
2025-09-14 23:40:15 +0100

    modified simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000  # Principal amount in dollars
 rate = 0.05          # Annual interest rate in percent 
 time = 3          # Time in years
 
-interest = (principal * rate * time)
+interest = (rate * time * principal)  # Calculating simple interest
 print(f"The simple interest is: {interest}") 
-# Formula: Simple Interest = (Principal * Rate * Time)
+
