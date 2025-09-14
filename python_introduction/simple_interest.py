34129f9472b0a1e62ddf5ae48fe0a4419f42bc26
Victor Nwachukwu <vixkudos@gmail.com>
2025-09-14 23:43:45 +0100

    modified simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,5 +3,5 @@ rate = 0.05          # Annual interest rate in percent
 time = 3          # Time in years
 
 interest = (rate * time * principal)  # Calculating simple interest
-print(f"The simple interest is: {interest}") 
+print("The simple interest is:", interest) 
 
