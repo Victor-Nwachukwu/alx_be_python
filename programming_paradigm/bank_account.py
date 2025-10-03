4860e7fefc48e76dcb307a7eda780eb1c1b250c6
Victor Nwachukwu <vixkudos@gmail.com>
2025-10-03 01:04:58 +0100

    Modified Simple Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -53,4 +53,4 @@ class BankAccount:
         """
         Display the current account balance in a user-friendly format.
         """
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
