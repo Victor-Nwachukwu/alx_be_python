9f9e7371f11656d77275f5902f8e6cb2bd9fa9bd
Victor Nwachukwu <vixkudos@gmail.com>
2025-10-04 06:03:52 +0100

    Modified simple test file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,46 +9,46 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     # ---------- Addition ----------
-    def test_add_positive_numbers(self):
+    def test_addition_positive_numbers(self):
         self.assertEqual(self.calc.add(5, 3), 8)
 
-    def test_add_negative_numbers(self):
+    def test_addition_negative_numbers(self):
         self.assertEqual(self.calc.add(-5, -3), -8)
 
-    def test_add_mixed_sign_numbers(self):
+    def test_addition_mixed_sign_numbers(self):
         self.assertEqual(self.calc.add(-5, 3), -2)
 
     # ---------- Subtraction ----------
-    def test_subtract_positive_numbers(self):
+    def test_subtraction_positive_numbers(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
 
-    def test_subtract_negative_numbers(self):
+    def test_subtraction_negative_numbers(self):
         self.assertEqual(self.calc.subtract(-5, -2), -3)
 
-    def test_subtract_resulting_in_negative(self):
+    def test_subtraction_resulting_in_negative(self):
         self.assertEqual(self.calc.subtract(3, 10), -7)
 
     # ---------- Multiplication ----------
-    def test_multiply_positive_numbers(self):
+    def test_multiplication_positive_numbers(self):
         self.assertEqual(self.calc.multiply(6, 7), 42)
 
-    def test_multiply_with_zero(self):
+    def test_multiplication_with_zero(self):
         self.assertEqual(self.calc.multiply(10, 0), 0)
 
-    def test_multiply_negative_numbers(self):
+    def test_multiplication_negative_numbers(self):
         self.assertEqual(self.calc.multiply(-3, 4), -12)
 
     # ---------- Division ----------
-    def test_divide_positive_numbers(self):
+    def test_division_positive_numbers(self):
         self.assertEqual(self.calc.divide(20, 5), 4)
 
-    def test_divide_negative_numbers(self):
+    def test_division_negative_numbers(self):
         self.assertEqual(self.calc.divide(-15, 3), -5)
 
-    def test_divide_by_zero(self):
+    def test_division_by_zero(self):
         self.assertIsNone(self.calc.divide(10, 0))  # expect None on divide by zero
 
-    def test_divide_resulting_in_float(self):
+    def test_division_resulting_in_float(self):
         self.assertEqual(self.calc.divide(7, 2), 3.5)
 
 
